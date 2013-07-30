052d8368a660a2953f3cc797fb1fdf42337f5190
laurabreiman <lauracle@mit.edu>
2013-07-30 10:50:54 -0400

    merging away

--- data.js
+++ data.js
@@ -5,14 +5,14 @@ var tempScale={"steak":
                 {'info':'Glycogen breaks down 55-60\xB0 C','position':3},
                 {'info':'Myoglobin denatures 60-70\xB0 C','position':2},
                 {'info':'Actin denatures 70-120\xB0 C','position':1},
-                {'info':'Browning reactions occur 120\xB0 C+','position':0}],
+                {'info':'Browning reactions occur 120+\xB0 C','position':0}],
               "fish":
                 [{'info':'lack of protein denaturization below 40\xB0 C','position':5},
                 {'info':'Myosin breaks down 40-50\xB0 C','position':4},
                 {'info':'Myocommata begins to rupture 50\xB0 C','position':3},
                 {'info':'Myoglobin denatures 60-70\xB0 C','position':2},
                 {'info':'Actin denatures 70\xB0 C','position':1},
-                {'info':'Fibers begin to disintegrate 90\xB0 C+','position':0}]
+                {'info':'Fibers begin to disintegrate 90+\xB0 C+','position':0}]
               };
 
 //color
@@ -24,11 +24,15 @@ var color = {
         "fish":
             d3.scale.linear()
             .domain([0,1,5,9,10])
-            .range(['#693d34' ,"#F58282","#f52900","#F58282",'#693d34']),
+            .range(['#692901','#FAEEE6','#FA3491', '#FAEEE6','#692901']),
         "turkey":
             d3.scale.linear()
             .domain([0,1,5,9,10])
-            .range(['#693d34' ,"#F58282","#f52900","#F58282",'#693d34'])
+            .range(['#692901','#FAEEE6','#B56980','#FAEEE6','#692901']),
+        "tofu":
+            d3.scale.linear()
+            .domain([0,1,5,9,10])
+            .range(['#DE843A','#F7DEC8','#F5F2F0','#F7DEC8','#DE843A'])
 };
 //boundaries where color change occurs
 var boundaries= {"steak":[120,70,60,55,40], "fish": [90,70,60,50,40]};
