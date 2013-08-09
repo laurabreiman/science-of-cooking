75b01af7e207cd873c841b5495ee6a6887c9229d
laurabreiman <lauracle@mit.edu>
2013-08-09 10:38:39 -0400

    parseRecipe function

--- sample.js
+++ sample.js
@@ -5,7 +5,6 @@
         with the function graphSteak
 */
 var calculate=function(data,steak,meatType,first,totalTime){
-    
     //sets the parameters for the iterations of the diffusion equations. 
     //parameters are initially set to a timestep of 1 second and a spacestep of 1mm,
     //but these can be increased with minimal error if calculation time is too great
